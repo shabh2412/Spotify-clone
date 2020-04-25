567d2e7be52579f12aa54628bcd81c3cf8d486e2
Rishabh Panesar <rishabhpanesar@gmail.com>
2020-04-25 21:00:28 +0530

    Created static variables to store error messages, and replaced all hardcoded strings

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -29,25 +29,25 @@
         }
         private function validateUsername($userName){
             if(strlen($userName) > 25 || strlen($userName) < 5){
-                array_push($this->errorArray, "Your username must be between 5 and 25 characters");
+                array_push($this->errorArray, Constants::$usernameCharacterLength);
                 return;
             } 
             //TODO : check if username exists
         }
         private function validateName($name){
             if(strlen($name) > 25 || strlen($name) < 2){
-                array_push($this->errorArray, "Your First and Last Name must be between 2 and 25 characters");
+                array_push($this->errorArray, Constants::$nameCharacterLength);
                 return;
             } 
     
         }
         private function validateEmail($em1 , $em2){
             if ($em1 != $em2) {
-                array_push($this->errorArray, "Both the email need to match");
+                array_push($this->errorArray, Constants::$emailDoNotMatch);
                 return;
             } else{
                 if(!preg_match($this->emVal, $em1)){
-                    array_push($this->errorArray, "Email is invalid");
+                    array_push($this->errorArray, Constants::$emailInvalid);
                     return;
                 }
                 // TODO : Check that email id hasn't already been used.
@@ -56,11 +56,15 @@
         }
         private function validatePassword($pwd1, $pwd2){
             if($pwd1 != $pwd2) {
-                array_push($this -> errorArray, "Both password need to match");
+                array_push($this -> errorArray, Constants::$passwordsDoNotMatch);
                 return;
             } else {
+                if(strlen($pwd1) < 8){
+                    array_push($this -> errorArray, Constants::$passwordTooShort);
+                }
                 if (!preg_match($this -> pwdVal, $pwd1)) {
-                    array_push( $this -> errorArray, "Password needs to consist of a lowercase, uppercase, numeric and a \"! or ?\"");
+                    array_push( $this -> errorArray, Constants::$passwordTooSimple);
+                    return;
                 }
             }
         }
