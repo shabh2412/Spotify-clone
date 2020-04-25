0d1f7b35d70ec3eeb736e79c6d8e6170ad2509cd
Rishabh <rishabhpanesar@gmail.com>
2020-04-25 17:41:56 +0530

    Added regex validation for email, and password

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,5 +1,7 @@
 <?php
     class Account {
+        private $emVal = '/^[a-zA-Z0-9_.-]+@[a-z.-]{3,16}\.[a-z]{2,5}/';
+        private $pwdVal = '/(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[!?])[a-zA-Z0-9!?]{8,}/';
         private $errorArray;
         public function __construct(){
             $this -> errorArray = array();
@@ -20,13 +22,34 @@
             //TODO : check if username exists
         }
         private function validateName($name){
+            if(strlen($name) > 25 || strlen($name) < 2){
+                array_push($this->errorArray, "Your First and Last Name must be between 2 and 25 characters");
+                return;
+            } 
     
         }
         private function validateEmail($em1 , $em2){
-    
+            if ($em1 != $em2) {
+                array_push($this->errorArray, "Both the email need to match");
+                return;
+            } else{
+                if(!preg_match($this->emVal, $em1)){
+                    array_push($this->errorArray, "Email is invalid");
+                    return;
+                }
+                // TODO : Check that email id hasn't already been used.
+            }
+
         }
         private function validatePassword($pwd1, $pwd2){
-    
+            if($pwd1 != $pwd2) {
+                array_push($this -> errorArray, "Both password need to match");
+                return;
+            } else {
+                if (!preg_match($this -> pwdVal, $pwd1)) {
+                    array_push( $this -> errorArray, "Password needs to consist of a lowercase, uppercase, numeric and a special character(\"! or ?\")");
+                }
+            }
         }
     }
 ?>
