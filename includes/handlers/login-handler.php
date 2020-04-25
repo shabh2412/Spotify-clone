d51e69ad0ead59ca634ca226a358dc18161c8757
Rishabh <rishabhpanesar@gmail.com>
2020-04-26 02:22:28 +0530

    Writing script for login

--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -1,6 +1,10 @@
 <?php
     if(isset($_POST['loginButton'])){
         // Login Button was pressed
+        $username = $_POST['loginUsername'];
+        $password = $_POST['loginPassword'];
+
+        //login function
         
     }
 ?>
