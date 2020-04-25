934291f06a818d284b5846cb6ae7f73140daf43a
Rishabh <rishabhpanesar@gmail.com>
2020-04-26 02:17:59 +0530

    Segregated different type of error msgs

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -1,13 +1,22 @@
 <?php class Constants {
-
+    
+    // password err msgs
     public static $passwordsDoNotMatch = "Both password need to match";
     public static $passwordTooShort = "Password needs to be of at least 8 digits!";
     public static $passwordTooSimple = "Password needs to consist of a lowercase, uppercase, numeric and a \"! or ?\"";
+
+
+    // email err msgs
     public static $emailInvalid = "Email is invalid";
     public static $emailDoNotMatch = "Both the email need to match";
+    public static $emailExists = "This email is already in use";
+    
+
+    // username err msgs
     public static $nameCharacterLength = "Your First and Last Name must be between 2 and 25 characters";
     public static $usernameCharacterLength = "Your username must be between 5 and 25 characters";
-
+    public static $usernameTaken = "Username already exists";
+    
 
 
 }
