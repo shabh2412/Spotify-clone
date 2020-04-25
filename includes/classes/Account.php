2ddac33b813a322ae8554e6111a41f0f6c2a46de
Rishabh <rishabhpanesar@gmail.com>
2020-04-25 15:14:55 +0530

    Added methods to call the private function from the parameterized constructor

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,11 +1,15 @@
 <?php
     class Account {
-        public function __construct()
+        public function __construct($userName,$firstName,$lastName,$email,$email_conf,$password,$password_conf)
         {
-            
+            $this -> validateUsername($userName);
+            $this -> validateName($firstName);
+            $this -> validateName($lastName);
+            $this -> validateEmail($email,$email_conf);
+            $this -> validatePassword($password, $password_conf);
         }
         private function validateUsername($username){
-
+            echo "username function called";
         }
         private function validateName($name){
     
