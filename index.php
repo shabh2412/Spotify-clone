0caefc9677cbdd6caf4c97b99484f145af5344fc
Rishabh <rishabhpanesar@gmail.com>
2020-04-27 01:31:42 +0530

    added nowPlayingBar container

--- index.php
+++ index.php
@@ -19,8 +19,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome To Muscify</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     
+    <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBar">
+            <div id="nowPlayingLeft" ></div>
+            <div id="nowPlayingCenter" ></div>
+            <div id="nowPlayingRight" ></div>
+        </div>
+    </div>
+
+
 </body>
 </html>
