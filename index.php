81640f42655d11b1a6ef2d9840af3aaf784cde9e
Rishabh <rishabhpanesar@gmail.com>
2020-04-26 21:49:20 +0530

    changed name to muscify

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome To Spotify Clone</title>
+    <title>Welcome To Muscify</title>
 </head>
 <body>
     
--- register.php
+++ register.php
@@ -16,7 +16,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Songtify</title>
+    <title>Welcome to Muscify</title>
     <link rel="stylesheet" href="assets/css/register.css">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
