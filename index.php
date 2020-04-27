679ef65220c7cb05df272f594d2c6427a6d6f84a
Rishabh <rishabhpanesar@gmail.com>
2020-04-27 18:09:14 +0530

    Added now plaing container

--- index.php
+++ index.php
@@ -26,7 +26,30 @@
     <div id="nowPlayingBarContainer">
         <div id="nowPlayingBar">
             <div id="nowPlayingLeft" ></div>
-            <div id="nowPlayingCenter" ></div>
+            <div id="nowPlayingCenter" >
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <button class="controlButton shuffle" title="Shuffle Button">
+                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                        </button>
+                        <button class="controlButton previous" title="Previous Button">
+                            <img src="assets/images/icons/previous.png" alt="Previous">
+                        </button>
+                        <button class="controlButton play" title="Play Button">
+                            <img src="assets/images/icons/play.png" alt="Play">
+                        </button>
+                        <button class="controlButton pause" title="Pause Button" style="display: none;">
+                            <img src="assets/images/icons/pause.png" alt="Pause">
+                        </button>
+                        <button class="controlButton next" title="Next Button">
+                            <img src="assets/images/icons/next.png" alt="Next">
+                        </button>
+                        <button class="controlButton repeat" title="repeat Button">
+                            <img src="assets/images/icons/repeat.png" alt="Repeat">
+                        </button>
+                    </div>
+                </div>
+            </div>
             <div id="nowPlayingRight" ></div>
         </div>
     </div>
