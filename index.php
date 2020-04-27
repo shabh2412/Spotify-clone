d0b976076a20ab63580ffbde094e5fa1d1acb831
Rishabh <rishabhpanesar@gmail.com>
2020-04-27 23:29:59 +0530

    added playback bar, progress bar

--- index.php
+++ index.php
@@ -48,6 +48,15 @@
                             <img src="assets/images/icons/repeat.png" alt="Repeat">
                         </button>
                     </div>
+                    <div class="playbackBar">
+                        <span class="progressTime current">0.00</span>
+                        <div class="progressBar">
+                            <div class="progressBarBg">
+                                <div class="progress"></div>
+                            </div>
+                        </div>
+                        <span class="progressTime remaining">0.00</span>
+                    </div>
                 </div>
             </div>
             <div id="nowPlayingRight" ></div>
