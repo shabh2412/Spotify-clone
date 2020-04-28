9a312ec090d53e97baf08877d42276142022417f
Rishabh <rishabhpanesar@gmail.com>
2020-04-28 19:34:56 +0530

    Fetching title from DB

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php include("includes/header.php");?>
 
+<h1 class="pageHeadingBig">You Might Also Like</h1>
+
+<div class="gridViewContainer">
+    <?php 
+        $albumQuery = "SELECT * FROM albums";
+        $resultAlbumQuery = mysqli_query($conn, $albumQuery);
+        while($row = mysqli_fetch_array($resultAlbumQuery)) {
+            echo $row['title']."<br>";
+        }
+    ?>
+</div>
+
+
+
 <?php include("includes/footer.php")?>
