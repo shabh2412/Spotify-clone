33f458c3a0fe6a7cadcdbb12e61ff96d2dd179eb
Rishabh <rishabhpanesar@gmail.com>
2020-04-28 17:18:12 +0530

    separated navBar and nowPlayingBar in diff files

---
+++ includes/navBarContainer.php
@@ -0,0 +1,30 @@
+<div id="navBarContainer">
+    <nav class="navBar">
+        <a href="index.php" class="logo">
+            <img src="assets/images/icons/logo-1.png" alt="Muscify">
+        </a>
+
+        <div class="group">
+            <div class="navItem">
+                <a href="search.php" class="navItemLink">Search
+                    <img src="assets/images/icons/search.png" alt="Search" title="Search" class="icon">
+                </a>
+            </div>
+        </div>
+        
+        <div class="group">
+            <div class="navItem">
+                <a href="browse.php" class="navItemLink">Browse</a>
+            </div>
+
+            <div class="navItem">
+                <a href="yourMusic.php" class="navItemLink">Your Music</a>
+            </div>
+
+            <div class="navItem">
+                <a href="profile.php" class="navItemLink">Profile</a>
+            </div>
+        </div>
+
+    </nav>
+</div>
