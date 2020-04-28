458592543491905c9545f45d86a1a2e6709d9c22
Rishabh <rishabhpanesar@gmail.com>
2020-04-28 21:16:33 +0530

    created Artist class

--- album.php
+++ album.php
@@ -10,12 +10,12 @@
     $albumQuery = "SELECT * FROM albums WHERE id = '$albumId'";
     $resultAlbumQuery = mysqli_query($conn, $albumQuery);
     $album = mysqli_fetch_array($resultAlbumQuery);
-    $artistId = $album['artist'];
-    $artistQuery = "SELECT * FROM artists WHERE id = '$artistId'";
-    $resultArtistQuery = mysqli_query($conn, $artistQuery);
-    $artist = mysqli_fetch_array($resultArtistQuery);
+
+    $artist = new Artist($conn, $album['artist']);
+
+
     echo $album['title']."<br>";
-    echo $artist['name'];
+    echo $artist -> getName();
 
 ?>
 
