a84569337b4e9c4e11b1af51e7d423ed640f57fc
Rishabh <rishabhpanesar@gmail.com>
2020-04-28 16:09:08 +0530

    Added topContainer, nav bar

--- index.php
+++ index.php
@@ -24,6 +24,40 @@
 <body>
     
     <div id="mainContainer">
+        <div id="topContainer">
+            <div id="navBarContainer">
+                <nav class="navBar">
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/icons/logo-1.png" alt="Muscify">
+                    </a>
+
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Search
+                                <img src="assets/images/icons/search.png" alt="Search" title="Search" class="icon">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="profile.php" class="navItemLink">Profile</a>
+                        </div>
+                        
+                    </div>
+
+
+
+
+                    
+                </nav>
+            </div>
+        </div>
         <div id="nowPlayingBarContainer">
             <div id="nowPlayingBar">
                 <div id="nowPlayingLeft" >
