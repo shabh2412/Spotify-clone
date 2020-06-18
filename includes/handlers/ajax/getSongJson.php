f02cf8a55f5c1e7534f10e2465a9b57e0c9c1dfa
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-06-18 11:58:17 +0530

    created a ajax file, to randomly select and return a song from DB

--- includes/handlers/ajax/getSongJson.php
+++ includes/handlers/ajax/getSongJson.php
@@ -1,3 +1,12 @@
 <?php
-    echo"Hello world";
+    include('../../config.php');
+    if(isset($_POST['songId'])){
+        $songId = $_POST['songId'];
+        $songQuery = "SELECT * FROM songs WHERE id = '$songId'";
+        $resultSongQuery = mysqli_query($conn, $songQuery);
+        $resultArray = mysqli_fetch_array($resultSongQuery);
+        $songJsonList = json_encode($resultArray);
+        echo $songJsonList;
+    }
+
 ?>
