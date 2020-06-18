d036aa1da9c2a925953b4e6e6b05e9336edddaa9
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-06-18 09:37:29 +0530

    Added jQuery

--- includes/header.php
+++ includes/header.php
@@ -23,6 +23,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome To Musify</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="assets/js/script.js"></script>
 </head>
 <body>
