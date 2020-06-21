856ea83db908e20d8ce12727897ff99c689872b9
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-06-21 19:30:46 +0530

    display duration of the song

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
 var currentPlaylist = Array();
 var audioElement;
 
+function formatTime(seconds){
+    var time = Math.round(seconds);
+    var minutes = Math.floor(time/60); //rounds down
+    var seconds = time - (minutes * 60);
+    if(seconds<10){
+        seconds = "0" + seconds;
+    }
+    return minutes + ":" + seconds;
+}
+
 function Audio() {
 
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
 
     this.audio.addEventListener("canplay", function(){
-        $(".progressTime.remaining").text(this.duration);
+        var duration = formatTime(this.duration)
+        $(".progressTime.remaining").text(duration);
     });
 
     this.setTrack = function (track) {
