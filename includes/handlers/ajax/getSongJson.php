0ae303769376348aa5243b59c7f5a507eb777cb9
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-03 20:36:46 +0530

    Changed songs to Songs

--- includes/handlers/ajax/getSongJson.php
+++ includes/handlers/ajax/getSongJson.php
@@ -2,7 +2,7 @@
     include('../../config.php');
     if(isset($_POST['songId'])){
         $songId = $_POST['songId'];
-        $songQuery = "SELECT * FROM songs WHERE id = '$songId'";
+        $songQuery = "SELECT * FROM Songs WHERE id = '$songId'";
         $resultSongQuery = mysqli_query($conn, $songQuery);
         $resultArray = mysqli_fetch_array($resultSongQuery);
         $songJsonList = json_encode($resultArray);
