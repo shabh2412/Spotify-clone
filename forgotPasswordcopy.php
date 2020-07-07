c2fb7e51dc85a330a3b791f453e178f99fc3c26e
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-07 20:06:15 +0530

    testing

--- forgotPasswordcopy.php
+++ forgotPasswordcopy.php
@@ -1,3 +1,65 @@
+<?php
+    echo "<script>
+                $(document).ready(function() {
+                    $('#loginForm').show();
+                });
+            </script>";
+?>
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    require("/var/www/html/PHPMailer/src/PHPMailer.php");   
+    require("/var/www/html/PHPMailer/src/SMTP.php");
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+    use PHPMailer\PHPMailer\SMTP;
+
+    // Load Composer's autoloader
+    require 'vendor/autoload.php';
+
+    // Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+    
+    $usrEmail = 'rishabhpanesar@gmail.com';
+    $userName = 'Rishabh Panesar';
+    
+    $user = "opensongsmusify@gmail.com";
+    
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = $user;                     // SMTP username
+        $mail->Password   = 'S2yi8bGEp45FLQU';                               // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+        //Recipients
+        $mail->setFrom($user, 'Musify Support');
+        $mail->addAddress($usrEmail, $userName);     // Add a recipient
+        $mail->addReplyTo($user, 'Musify Support');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
+
+        // Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Reset Password';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo '<script>console.log(Message has been sent);</script>';
+    } catch (Exception $e) {
+        echo "<script>console.log(Message could not be sent. Mailer Error: {$mail->ErrorInfo});</script>";
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -30,67 +92,5 @@
         </div>
     </body>
 </html>
-    <?php
-        echo "<script>
-                    $(document).ready(function() {
-                        $('#loginForm').show();
-                    });
-                </script>";
-    ?>
-    <?php
-        use PHPMailer\PHPMailer\PHPMailer;
-        use PHPMailer\PHPMailer\Exception;
-        require("/var/www/html/PHPMailer/src/PHPMailer.php");   
-        require("/var/www/html/PHPMailer/src/SMTP.php");
-        require 'PHPMailer/src/Exception.php';
-        require 'PHPMailer/src/PHPMailer.php';
-        require 'PHPMailer/src/SMTP.php';
-        use PHPMailer\PHPMailer\SMTP;
-
-        // Load Composer's autoloader
-        require 'vendor/autoload.php';
-
-        // Instantiation and passing `true` enables exceptions
-        $mail = new PHPMailer(true);
-        
-        $usrEmail = 'rishabhpanesar@gmail.com';
-        $userName = 'Rishabh Panesar';
-        
-        $user = "opensongsmusify@gmail.com";
-        
-        try {
-            //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-            $mail->isSMTP();                                            // Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = $user;                     // SMTP username
-            $mail->Password   = 'S2yi8bGEp45FLQU';                               // SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-            //Recipients
-            $mail->setFrom($user, 'Musify Support');
-            $mail->addAddress($usrEmail, $userName);     // Add a recipient
-            $mail->addReplyTo($user, 'Musify Support');
-            // $mail->addCC('cc@example.com');
-            // $mail->addBCC('bcc@example.com');
-
-            // Attachments
-            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-            // Content
-            $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = 'Reset Password';
-            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-            $mail->send();
-            echo '<script>console.log(Message has been sent);</script>';
-        } catch (Exception $e) {
-            echo "<script>console.log(Message could not be sent. Mailer Error: {$mail->ErrorInfo});</script>";
-        }
-    ?>
 
 
