dbd8773fcff8a106fff5e41431f9d3505fe9ca21
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-07 18:57:10 +0530

    testing

--- forgotPasswordcopy.php
+++ forgotPasswordcopy.php
@@ -1,4 +1,39 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Reset Password</title>
+        <link rel="stylesheet" href="assets/css/register.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
+    </head>
+    <body>  
+        <div id="background">
+            <div id="loginContainer">
+                <div id="inputContainer">
+                    <form id='loginForm'>
+                    <h2>Reset Your Password</h2>
+                        <p>
+                            <label for="loginUsername">Username</label>
+                            <input type="text" name="loginUsername" id="loginUsername" placeholder="Ex: johnDoe" required>
+                        </p>
+                        <p>
+                            <label for="loginEmail">Your E-Mail ID</label>
+                            <input type="email" name="loginEmail" id="loginEmail" placeholder="johndoe@example.com" required>
+                        </p>
+                        <button type="submit" value='Reset Pwd' name="loginButton">Reset Password</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    <?php
+        echo "<script>
+                    $(document).ready(function() {
+                        $('#loginForm').show();
+                    });
+                </script>";
+    ?>
+    <?php
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
     require("/home/site/libs/PHPMailer-master/src/PHPMailer.php");   require("/home/site/libs/PHPMailer-master/src/SMTP.php");
@@ -52,41 +87,6 @@
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Reset Password</title>
-        <link rel="stylesheet" href="assets/css/register.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
-    </head>
-    <body>  
-        <div id="background">
-            <div id="loginContainer">
-                <div id="inputContainer">
-                    <form id='loginForm'>
-                    <h2>Reset Your Password</h2>
-                        <p>
-                            <label for="loginUsername">Username</label>
-                            <input type="text" name="loginUsername" id="loginUsername" placeholder="Ex: johnDoe" required>
-                        </p>
-                        <p>
-                            <label for="loginEmail">Your E-Mail ID</label>
-                            <input type="email" name="loginEmail" id="loginEmail" placeholder="johndoe@example.com" required>
-                        </p>
-                        <button type="submit" value='Reset Pwd' name="loginButton">Reset Password</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    <?php
-        echo "<script>
-                    $(document).ready(function() {
-                        $('#loginForm').show();
-                    });
-                </script>";
-    ?>
     </body>
 </html>
 
