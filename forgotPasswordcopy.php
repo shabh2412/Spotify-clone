d2e70b9d33b047d40ae806b9b08e6235d1052c08
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-07 20:16:59 +0530

    testing

--- forgotPasswordcopy.php
+++ forgotPasswordcopy.php
@@ -1,8 +1,6 @@
 <?php
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
-    // require("/var/www/html/PHPMailer/src/PHPMailer.php");   
-    // require("/var/www/html/PHPMailer/src/SMTP.php");
     require 'PHPMailer/src/Exception.php';
     require 'PHPMailer/src/PHPMailer.php';
     require 'PHPMailer/src/SMTP.php';
