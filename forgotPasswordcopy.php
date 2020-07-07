20e39217e22d8410095fdcc81ee996f90c831d61
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-07 17:54:44 +0530

    testing

---
+++ forgotPasswordcopy.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Reset Password</title>
+        <link rel="stylesheet" href="assets/css/register.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
+    </head>
+    <body>
+    <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+
+    if(isset($_POST['loginEmail']) && isset($_POST['loginUsername']) ){
+        $sendTo = $_POST['loginEmail'];
+        $userName = $_POST['loginUsername'];
+        // Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'opensongsmusify@gmail.com';                     // SMTP username
+            $mail->Password   = 'S2yi8bGEp45FLQU';                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+            //Recipients
+            $mail->setFrom('support@musifysongs.com', 'Musify Support');
+            $mail->addAddress($sendTo, 'Rishabh Panesar');     // Add a recipient
+            $mail->addReplyTo('opensongsmusify@gmail.com', 'Musify Support');
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Password Reset';
+            $mail->Body    = 'Hi '.$userName.'!,<br> <b>This is a mail regarding password reset!</b> HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo '<script>alert("Message has been sent")</script>';
+            echo "Mail Sent Successfully! <br><a href='register.php'>Login Now</a>";
+            // exit();
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+?>
+        <div id="background">
+            <div id="loginContainer">
+                <div id="inputContainer">
+                </div>
+            </div>
+        </div>
+    <?php
+        echo "<script>
+                    $(document).ready(function() {
+                        $('#loginForm').show();
+                    });
+                </script>";
+    ?>
+    </body>
+</html>
+
