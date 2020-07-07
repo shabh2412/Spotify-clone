dad8b9aee83dc2417965704fb09cbb58b789a5e3
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-07 18:59:22 +0530

    testing

--- forgotPasswordcopy.php
+++ forgotPasswordcopy.php
@@ -34,59 +34,59 @@
                 </script>";
     ?>
     <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-    require("/home/site/libs/PHPMailer-master/src/PHPMailer.php");   require("/home/site/libs/PHPMailer-master/src/SMTP.php");
-    require 'PHPMailer/src/Exception.php';
-    require 'PHPMailer/src/PHPMailer.php';
-    require 'PHPMailer/src/SMTP.php';
-    use PHPMailer\PHPMailer\SMTP;
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\Exception;
+        require("/home/site/libs/PHPMailer-master/src/PHPMailer.php");   require("/home/site/libs/PHPMailer-master/src/SMTP.php");
+        require 'PHPMailer/src/Exception.php';
+        require 'PHPMailer/src/PHPMailer.php';
+        require 'PHPMailer/src/SMTP.php';
+        use PHPMailer\PHPMailer\SMTP;
 
-    // Load Composer's autoloader
-    // require 'vendor/autoload.php';
+        // Load Composer's autoloader
+        require 'vendor/autoload.php';
 
-    // Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
+        // Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
 
-    $usrEmail = 'rishabhpanesar@gmail.com';
-    $userName = 'Rishabh Panesar';
+        $usrEmail = 'rishabhpanesar@gmail.com';
+        $userName = 'Rishabh Panesar';
 
-    $user = "opensongsmusify@gmail.com";
+        $user = "opensongsmusify@gmail.com";
 
-    try {
-        //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = $user;                     // SMTP username
-        $mail->Password   = 'S2yi8bGEp45FLQU';                               // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = $user;                     // SMTP username
+            $mail->Password   = 'S2yi8bGEp45FLQU';                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-        //Recipients
-        $mail->setFrom($user, 'Musify Support');
-        $mail->addAddress($usrEmail, $userName);     // Add a recipient
-        $mail->addReplyTo($user, 'Musify Support');
-        // $mail->addCC('cc@example.com');
-        // $mail->addBCC('bcc@example.com');
+            //Recipients
+            $mail->setFrom($user, 'Musify Support');
+            $mail->addAddress($usrEmail, $userName);     // Add a recipient
+            $mail->addReplyTo($user, 'Musify Support');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
 
-        // Attachments
-        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+            // Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-        // Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Reset Password';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Reset Password';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
-?>
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    ?>
     </body>
 </html>
 
