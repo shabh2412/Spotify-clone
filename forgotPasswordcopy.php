e4b5a02bc1c35ae0a18ee0b038a7d137f2ffd4db
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-07 18:50:07 +0530

    testing

--- forgotPasswordcopy.php
+++ forgotPasswordcopy.php
@@ -2,9 +2,9 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require 'path/to/PHPMailer/src/Exception.php';
-    require 'path/to/PHPMailer/src/PHPMailer.php';
-    require 'path/to/PHPMailer/src/SMTP.php';
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
     use PHPMailer\PHPMailer\SMTP;
 
     // Load Composer's autoloader
