03b42486e9e1b2956444abc0471665e99e384c5e
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-07 18:38:25 +0530

    testing

--- forgotPasswordcopy.php
+++ forgotPasswordcopy.php
@@ -7,17 +7,39 @@
         <link rel="stylesheet" href="assets/css/register.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
     </head>
+    <body>  
+        <div id="background">
+            <div id="loginContainer">
+                <div id="inputContainer">
+                    <form id='loginForm'>
+                    <h2>Reset Your Password</h2>
+                        <p>
+                            <label for="loginUsername">Username</label>
+                            <input type="text" name="loginUsername" id="loginUsername" placeholder="Ex: johnDoe" required>
+                        </p>
+                        <p>
+                            <label for="loginEmail">Your E-Mail ID</label>
+                            <input type="email" name="loginEmail" id="loginEmail" placeholder="johndoe@example.com" required>
+                        </p>
+                        <button type="submit" value='Reset Pwd' name="loginButton">Reset Password</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     <?php
-            use PHPMailer\PHPMailer\PHPMailer;
-            use PHPMailer\PHPMailer\Exception;
-            
-            require 'PHPMailer/src/Exception.php';
-            require 'PHPMailer/src/PHPMailer.php';
-            require 'PHPMailer/src/SMTP.php';
-            ?>
-    <body>
-        <p>
-            <?php
+        echo "<script>
+                    $(document).ready(function() {
+                        $('#loginForm').show();
+                    });
+                </script>";
+    ?>
+    <?php
+                use PHPMailer\PHPMailer\PHPMailer;
+                use PHPMailer\PHPMailer\Exception;
+                
+                require 'PHPMailer/src/Exception.php';
+                require 'PHPMailer/src/PHPMailer.php';
+                require 'PHPMailer/src/SMTP.php';
                 if(isset($_POST['loginEmail']) && isset($_POST['loginUsername']) ){
                     $sendTo = $_POST['loginEmail'];
                     $userName = $_POST['loginUsername'];
@@ -52,33 +74,7 @@
                         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
                     }
                 }
-            ?>
-        </p>
-        <div id="background">
-            <div id="loginContainer">
-                <div id="inputContainer">
-                    <form id='loginForm'>
-                    <h2>Reset Your Password</h2>
-                        <p>
-                            <label for="loginUsername">Username</label>
-                            <input type="text" name="loginUsername" id="loginUsername" placeholder="Ex: johnDoe" required>
-                        </p>
-                        <p>
-                            <label for="loginEmail">Your E-Mail ID</label>
-                            <input type="email" name="loginEmail" id="loginEmail" placeholder="johndoe@example.com" required>
-                        </p>
-                        <button type="submit" value='Reset Pwd' name="loginButton">Reset Password</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    <?php
-        echo "<script>
-                    $(document).ready(function() {
-                        $('#loginForm').show();
-                    });
-                </script>";
-    ?>
+        ?>
     </body>
 </html>
 
