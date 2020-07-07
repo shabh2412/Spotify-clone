f6967eebb19c5a37202ceff5ae7b0a3d23df7bf5
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-07 17:27:27 +0530

    testing

--- forgotPassword.php
+++ forgotPassword.php
@@ -1,61 +1,61 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
+    // use PHPMailer\PHPMailer\PHPMailer;
+    // use PHPMailer\PHPMailer\SMTP;
+    // use PHPMailer\PHPMailer\Exception;
 
-    require 'PHPMailer/src/Exception.php';
-    require 'PHPMailer/src/PHPMailer.php';
-    require 'PHPMailer/src/SMTP.php';
+    // require 'PHPMailer/src/Exception.php';
+    // require 'PHPMailer/src/PHPMailer.php';
+    // require 'PHPMailer/src/SMTP.php';
 
-    if(isset($_POST['loginEmail']) && isset($_POST['loginUsername']) ){
-        $sendTo = $_POST['loginEmail'];
-        $userName = $_POST['loginUsername'];
-        // Instantiation and passing `true` enables exceptions
-        $mail = new PHPMailer(true);
+    // if(isset($_POST['loginEmail']) && isset($_POST['loginUsername']) ){
+    //     $sendTo = $_POST['loginEmail'];
+    //     $userName = $_POST['loginUsername'];
+    //     // Instantiation and passing `true` enables exceptions
+    //     $mail = new PHPMailer(true);
 
-        try {
-            //Server settings
-            // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-            $mail->isSMTP();                                            // Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'opensongsmusify@gmail.com';                     // SMTP username
-            $mail->Password   = 'S2yi8bGEp45FLQU';                               // SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    //     try {
+    //         //Server settings
+    //         // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    //         $mail->isSMTP();                                            // Send using SMTP
+    //         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    //         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    //         $mail->Username   = 'opensongsmusify@gmail.com';                     // SMTP username
+    //         $mail->Password   = 'S2yi8bGEp45FLQU';                               // SMTP password
+    //         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    //         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-            //Recipients
-            $mail->setFrom('support@musifysongs.com', 'Musify Support');
-            $mail->addAddress($sendTo, 'Rishabh Panesar');     // Add a recipient
-            // $mail->addAddress('ellen@example.com');               // Name is optional
-            $mail->addReplyTo('opensongsmusify@gmail.com', 'Musify Support');
-            // $mail->addCC('cc@example.com');
-            // $mail->addBCC('bcc@example.com');
+    //         //Recipients
+    //         $mail->setFrom('support@musifysongs.com', 'Musify Support');
+    //         $mail->addAddress($sendTo, 'Rishabh Panesar');     // Add a recipient
+    //         // $mail->addAddress('ellen@example.com');               // Name is optional
+    //         $mail->addReplyTo('opensongsmusify@gmail.com', 'Musify Support');
+    //         // $mail->addCC('cc@example.com');
+    //         // $mail->addBCC('bcc@example.com');
 
-            // Attachments
-            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    //         // Attachments
+    //         // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //         // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-            // Content
-            $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = 'Password Reset';
-            $mail->Body    = 'Hi '.$userName.'!,<br> <b>This is a mail regarding password reset!</b> HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    //         // Content
+    //         $mail->isHTML(true);                                  // Set email format to HTML
+    //         $mail->Subject = 'Password Reset';
+    //         $mail->Body    = 'Hi '.$userName.'!,<br> <b>This is a mail regarding password reset!</b> HTML message body <b>in bold!</b>';
+    //         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-            $mail->send();
-            echo '<script>alert("Message has been sent")</script>';
-            echo "Mail Sent Successfully! <br><a href='register.php'>Login Now</a>";
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
-        exit();
+    //         $mail->send();
+    //         echo '<script>alert("Message has been sent")</script>';
+    //         echo "Mail Sent Successfully! <br><a href='register.php'>Login Now</a>";
+    //     } catch (Exception $e) {
+    //         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    //     }
+    //     exit();
         
-    }    
-    function getInputValue($name) {
-        if(isset($_POST[$name])){
-            echo $_POST[$name];
-        }
-    }
+    // }    
+    // function getInputValue($name) {
+    //     if(isset($_POST[$name])){
+    //         echo $_POST[$name];
+    //     }
+    // }
 ?>
 <!DOCTYPE html>
 <html lang="en">
