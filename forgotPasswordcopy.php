d5e1b8a12e5587dc6955417756a20e6915b9db9c
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-07 18:51:42 +0530

    testing

--- forgotPasswordcopy.php
+++ forgotPasswordcopy.php
@@ -20,7 +20,7 @@
 
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
