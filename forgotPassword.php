ce832f63e30c11158c991a3d3a628b1691bb66b6
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-07 17:17:06 +0530

    Mailer working properly

--- forgotPassword.php
+++ forgotPassword.php
@@ -1,4 +1,4 @@
-<?php include('includes/header.php');
+<?php
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
@@ -7,42 +7,90 @@
     require 'PHPMailer/src/PHPMailer.php';
     require 'PHPMailer/src/SMTP.php';
 
-    // Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
+    if(isset($_POST['loginEmail']) && isset($_POST['loginUsername']) ){
+        $sendTo = $_POST['loginEmail'];
+        $userName = $_POST['loginUsername'];
+        // Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
 
-    try {
-        //Server settings
-        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'opensongsmusify@gmail.com';                     // SMTP username
-        $mail->Password   = 'S2yi8bGEp45FLQU';                               // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        try {
+            //Server settings
+            // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'opensongsmusify@gmail.com';                     // SMTP username
+            $mail->Password   = 'S2yi8bGEp45FLQU';                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-        //Recipients
-        $mail->setFrom('support@musifysongs.com', 'Musify Support');
-        $mail->addAddress('rishabhpanesar@gmail.com', 'Rishabh Panesar');     // Add a recipient
-        // $mail->addAddress('ellen@example.com');               // Name is optional
-        $mail->addReplyTo('opensongsmusify@gmail.com', 'Musify Support');
-        // $mail->addCC('cc@example.com');
-        // $mail->addBCC('bcc@example.com');
+            //Recipients
+            $mail->setFrom('support@musifysongs.com', 'Musify Support');
+            $mail->addAddress($sendTo, 'Rishabh Panesar');     // Add a recipient
+            // $mail->addAddress('ellen@example.com');               // Name is optional
+            $mail->addReplyTo('opensongsmusify@gmail.com', 'Musify Support');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
 
-        // Attachments
-        // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+            // Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-        // Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Password Reset';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Password Reset';
+            $mail->Body    = 'Hi '.$userName.'!,<br> <b>This is a mail regarding password reset!</b> HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-        $mail->send();
-        echo '<script>alert("Message has been sent")</script>';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            $mail->send();
+            echo '<script>alert("Message has been sent")</script>';
+            echo "Mail Sent Successfully! <br><a href='register.php'>Login Now</a>";
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+        exit();
+        
+    }    
+    function getInputValue($name) {
+        if(isset($_POST[$name])){
+            echo $_POST[$name];
+        }
     }
-    include('includes/footer.php');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="assets/css/register.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> 
+    </head>
+    <body>
+        <div id="background">
+            <div id="loginContainer">
+                <div id="inputContainer">
+                    <form id="loginForm" method="POST">
+                        <h2>Reset Your Password</h2>
+                        <p>
+                            <label for="loginUsername">Username</label>
+                            <input type="text" name="loginUsername" id="loginUsername" placeholder="Ex: johnDoe" value = "<?php getInputValue('Username');?>" required>
+                        </p>
+                        <p>
+                            <label for="loginEmail">Your E-Mail ID</label>
+                            <input type="email" name="loginEmail" id="loginEmail" placeholder="johndoe@example.com" value = "<?php getInputValue('email');?>" required>
+                        </p>
+                        <button type="submit" value='Reset Pwd' name="loginButton">Reset Password</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    <?php
+        echo "<script>
+                    $(document).ready(function() {
+                        $('#loginForm').show();
+                    });
+                </script>";
+    ?>
+    </body>
+</html>
