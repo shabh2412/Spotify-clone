7b737762a91ce428156a554707cff7dabdf1774e
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-12 20:50:12 +0530

    printing the songs in playlist

--- album.php
+++ album.php
@@ -62,7 +62,7 @@
         <script>
             var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
             tempPlaylist = JSON.parse(tempSongIds);
-            alert(tempPlaylist);
+            console.log("The Id's -> ",tempPlaylist);
         </script>
     </ul>
 </div>
