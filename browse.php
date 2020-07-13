36f5cb1478e55e6184bd10086a83b669cca3ea5a
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-13 08:47:51 +0530

    added browse page & edited index page

--- browse.php
+++ browse.php
@@ -2,7 +2,7 @@
     include("includes/includedFiles.php");
 ?>
 
-<h1 class="pageHeadingBig">Get Started With These Songs</h1>
+<h1 class="pageHeadingBig">You Might Also Like</h1>
 
 <div class="gridViewContainer">
     <?php 
