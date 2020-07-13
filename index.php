476828d49b8a514749f22e0de16169ba2861b273
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-13 08:50:14 +0530

    edited index page

--- index.php
+++ index.php
@@ -2,23 +2,4 @@
     include("includes/includedFiles.php");
 ?>
 
-<h1 class="pageHeadingBig">Get Started With These Songs</h1>
-
-<div class="gridViewContainer">
-    <?php 
-        $albumQuery = "SELECT * FROM albums ORDER BY RAND() LIMIT 10";
-        $resultAlbumQuery = mysqli_query($conn, $albumQuery);
-        while($row = mysqli_fetch_array($resultAlbumQuery)) {
-            echo "
-            <div class='gridViewItem'>
-                <span role='link' tabindex='0' onclick='openPage(\"album.php?id=".$row['id']."\")'>
-                    <img src=".$row['artworkPath']." alt=''>
-                    <div class='gridViewInfo'>
-                        ".$row['title']."
-                    </div>
-                </span>
-            </div>
-            ";
-        }
-        ?>
-</div>
+<script>openPage('browse.php');</script>
