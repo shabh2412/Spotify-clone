1b6ce880e78eba69b26e09351889b41c2cb4f7f9
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-13 09:01:17 +0530

    printing artist name

--- artist.php
+++ artist.php
@@ -11,5 +11,13 @@
 ?>
 
 <div class="entityInfo">
-    
+    <div class="centerSection">
+        <div class="artistInfo">
+            <h1 class="artistName">
+                <?php
+                    echo $artist->getName();
+                ?>
+            </h1>
+        </div>
+    </div>
 </div>
