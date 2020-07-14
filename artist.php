8ad2863c7745edafce441b5325aa25b0a8cf05e9
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-14 18:27:21 +0530

    added code to display popular songs of each artist

--- artist.php
+++ artist.php
@@ -24,3 +24,42 @@
         </div>
     </div>
 </div>
+<hr>
+<div class="tracklistContainer">
+    <ul class="tracklist">
+        <?php
+            $songIdArray = $artist->getSongIds();
+            $i = 1;
+            foreach($songIdArray as $songId){
+                $artistSong = new Song($conn, $songId);
+                $artistArtist = $artistSong->getArtist();
+                echo "<li class='tracklistRow'>
+                    <div class='trackCount'>
+                        <img src='assets/images/icons/play-white.png' alt='Play' class='play' onclick='setTrack(\"" . $artistSong->getId() . "\", tempPlaylist, true)'>
+                        <span class='trackNumber'>$i</span>
+                    </div>
+
+                    <div class='trackInfo'>
+                        <span class='trackName'>".$artistSong->getTitle()."</span>
+                        <span class='artistName'>".$artistArtist->getName()."</span>
+                    </div>
+                    
+                    <div class='trackOptions'>
+                        <img src='assets/images/icons/more.png' alt='More Info' class='optionsButton'>
+                    </div>
+
+                    <div class='trackDuration'>
+                        <span class='duration'>".$artistSong->getDuration()."</span>
+                    </div>
+
+                </li>";
+            $i = $i + 1;
+            }
+        ?>
+        <script>
+            var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+            tempPlaylist = JSON.parse(tempSongIds);
+            // console.log("The Id's -> ",tempPlaylist);  //For testing the playlist
+        </script>
+    </ul>
+</div>
