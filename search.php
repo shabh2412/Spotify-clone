48009c71439fff13f990607fcc0e9cf0bda6ed5d
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-19 14:44:53 +0530

    Wait for 2 seconds and then execute the code

--- search.php
+++ search.php
@@ -13,3 +13,15 @@
     </h4>
     <input type="text" class="searchInput" value="<?php echo $query;?>" placeholder="Start Typing">
 </div>
+
+<script>
+    $(function(){
+        var timer;
+        $(".searchInput").keyup(function () {
+            clearTimeout(timer);
+            timer = setTimeout(function() {
+                console.log("Hi!");
+            }, 2000); //wait 2 sec and execute the code.
+        });
+    });
+</script>
