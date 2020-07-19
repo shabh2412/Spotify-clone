2a914b274dd0c68fe61d3ddd45580583af009529
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-19 16:01:31 +0530

    implemented search for songs feature

--- search.php
+++ search.php
@@ -1,9 +1,9 @@
 <?php
     include("includes/includedFiles.php");
-    if(isset($_GET['query'])) {
-        $query = urldecode($_GET['query']);
+    if(isset($_GET['term'])) {
+        $term = urldecode($_GET['term']);
     } else {
-        $query = "";
+        $term = "";
     }
 ?>
 
@@ -11,7 +11,7 @@
     <h4>
         Search for an Artist, Album, or a Song
     </h4>
-    <input type="text" class="searchInput" value="<?php echo $query;?>" placeholder="Start Typing" onfocus="var val=this.value; this.value=''; this.value= val;">
+    <input type="text" class="searchInput" value="<?php echo $term;?>" placeholder="Start Typing" onfocus="var val=this.value; this.value=''; this.value= val;">
 </div>
 
 <script>
@@ -22,8 +22,56 @@
             clearTimeout(timer);
             timer = setTimeout(function() {
                 var val = $(".searchInput").val();
-                openPage("search.php?query=" + val);
+                openPage("search.php?term=" + val);
             }, 2000); //wait 2 sec and execute the code.
         });
     });
 </script>
+
+<div class="tracklistContainer borderBottom">
+    <h2>Trending Songs of this Artist</h2>
+    <ul class="tracklist">
+        <?php
+            $songsQuery = mysqli_query($conn, "SELECT id FROM Songs WHERE title LIKE '$term%' LIMIT 10");
+            if(mysqli_num_rows($songsQuery) == 0) {
+                echo "<span class='noResults'> No Songs found matching". $term ."</span>" ; 
+            }
+            $songIdArray = array();
+            $i = 1;
+            while($row = mysqli_fetch_array($songsQuery)){
+                if($i > 15){
+                    break;
+                }
+                array_push($songIdArray, $row['id']);
+                $artistSong = new Song($conn, $row['id']);
+                $artistArtist = $artistSong->getArtist();
+                echo "<li class='tracklistRow'>
+                    <div class='trackCount'>
+                        <img src='assets/images/icons/play-white.png' alt='Play' class='play' onclick='setTrack(\"" . $artistSong->getId() . "\", tempPlaylist, true)'>
+                        <span class='trackNumber'>$i</span>
+                    </div>
+
+                    <div class='trackInfo'>
+                        <span class='trackName'>".$artistSong->getTitle()."</span>
+                        <span class='artistName'>".$artistArtist->getName()."</span>
+                    </div>
+                    
+                    <div class='trackOptions'>
+                        <img src='assets/images/icons/more.png' alt='More Info' class='optionsButton'>
+                    </div>
+
+                    <div class='trackDuration'>
+                        <span class='duration'>".$artistSong->getDuration()."</span>
+                    </div>
+
+                </li>";
+            $i = $i + 1;
+            }
+        ?>
+        <script>
+            var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+            tempPlaylist = JSON.parse(tempSongIds);
+            // console.log("The Id's -> ",tempPlaylist);  //For testing the playlist
+        </script>
+    </ul>
+</div>
