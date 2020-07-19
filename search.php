db868964f37946f44d9645e63a5ac06f8d302855
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-19 15:08:45 +0530

    search when users stops typing

--- search.php
+++ search.php
@@ -11,16 +11,18 @@
     <h4>
         Search for an Artist, Album, or a Song
     </h4>
-    <input type="text" class="searchInput" value="<?php echo $query;?>" placeholder="Start Typing">
+    <input type="text" class="searchInput" value="<?php echo $query;?>" placeholder="Start Typing" onfocus="this.value = this.value">
 </div>
 
 <script>
+    $(".searchInput").focus();
     $(function(){
         var timer;
         $(".searchInput").keyup(function () {
             clearTimeout(timer);
             timer = setTimeout(function() {
-                console.log("Hi!");
+                var val = $(".searchInput").val();
+                openPage("search.php?query=" + val);
             }, 2000); //wait 2 sec and execute the code.
         });
     });
