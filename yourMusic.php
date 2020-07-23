15c19dc1a00a3dfd7351ccf73c2bb2a3f325fa32
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-23 23:28:49 +0530

    added heading and button

---
+++ yourMusic.php
@@ -0,0 +1,12 @@
+<?php
+    include("includes/includedFiles.php");
+?>
+
+<div class="playlistsContainer">
+    <div class="gridViewContainer">
+        <h2>Playlists</h2>
+        <div class="buttonItems">
+            <button class="button green">NEW PLAYLIST</button>
+        </div>
+    </div>
+</div>
