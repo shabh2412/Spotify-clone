2f543d49dca5bc4e537f48a06e42475164c55565
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-24 21:57:50 +0530

    corrected typo

--- includes/handlers/ajax/deletePlaylist.php
+++ includes/handlers/ajax/deletePlaylist.php
@@ -1,8 +1,8 @@
 <?php
     include("../../config.php");
 
-    if(isset($_GET['playlistId'])){
-        $playlistId = $_GET['playlistId'];
+    if(isset($_POST['playlistId'])){
+        $playlistId = $_POST['playlistId'];
         $playlistQuery = mysqli_query($conn, "DELETE FROM playlists WHERE id ='$playlistId'");
         $songsQuery = mysqli_query($conn, "DELETE FROM playlistSongs WHERE playlistId = '$playlistId'");
     } else {
