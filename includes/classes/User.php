b325fef94e42a557935e2076cdfdb7db3f8f3404
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-24 14:11:50 +0530

    created User class

---
+++ includes/classes/User.php
@@ -0,0 +1,10 @@
+<?php
+    class User {
+        private $conn;
+        private $username;
+        public function __construct($conn, $username){
+            $this->conn = $conn;
+            $this->username = $username;
+        }
+    }
+?>
--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -1,6 +1,7 @@
 <?php
     if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
         include("includes/config.php");
+        include("includes/classes/User.php");
         include("includes/classes/Artist.php");
         include("includes/classes/Album.php");
         include("includes/classes/Song.php");
