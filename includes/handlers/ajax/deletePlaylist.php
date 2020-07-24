8d370579f1008a512c9d1e6719c0f88ad133ef93
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-24 21:56:18 +0530

    created deletePlaylist file

---
+++ includes/handlers/ajax/deletePlaylist.php
@@ -0,0 +1,13 @@
+<?php
+    include("../../config.php");
+
+    if(isset($_GET['playlistId'])){
+        $playlistId = $_GET['playlistId'];
+        $playlistQuery = mysqli_query($conn, "DELETE FROM playlists WHERE id ='$playlistId'");
+        $songsQuery = mysqli_query($conn, "DELETE FROM playlistSongs WHERE playlistId = '$playlistId'");
+    } else {
+        echo "Oops, Something Went Wrong!";
+        echo "Playlist ID was not passed into this file!";
+    }
+
+?>
