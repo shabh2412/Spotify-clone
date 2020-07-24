bfc5641016d5d83367f74951954810d572e4a27d
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-24 17:00:42 +0530

    added dynamic link to the artists page

--- album.php
+++ album.php
@@ -23,7 +23,7 @@
                 echo $album->getTitle();
             ?>
         </h2>
-        <span>By <?php echo $artist->getName();?></span>
+        <p role="link" tabindex="0" onclick="openPage('artist.php?id=$artist->getId()')">By <?php echo $artist->getName();?></p>
         <p> <?php echo $album->getNumberOfSongs();?> Songs</p>
     </div>
 </div>
