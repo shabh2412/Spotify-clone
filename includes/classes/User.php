b23c2e23b6de462f9b2f53170e60f89d71be4479
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-26 15:33:06 +0530

    function to retrive name of user

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -10,5 +10,11 @@
         {
             return $this->username;
         }
+        public function getFirstAndLastName()
+        {
+            $query = mysqli_query($this->conn, "SELECT concat(firstName, ' ', lastName) AS name FROM users WHERE username = '$this->username'");
+            $row = mysqli_fetch_array($query);
+            return $row['name'];
+        }
     }
 ?>
