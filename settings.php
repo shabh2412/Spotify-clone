8f445ab276f7a58aedc9230219c3d35a771f6f61
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-26 15:36:29 +0530

    Greeting the user

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+    include("includes/includedFiles.php");
+?>
+
+<div class="entityInfo">
+    <div class="centerSection">
+        <div class="userInfo">
+            <h1><?php echo "Hi ". $userLoggedIn->getFirstAndLastName() . "!";?></h1>
+        </div>
+    </div>
+</div>
