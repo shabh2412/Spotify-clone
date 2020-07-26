287b143f3fb7dd66803f76c2d6a438b86dfc02c7
Rishabh Panesar <rishabh.panesar2609@gmail.com>
2020-07-26 17:05:23 +0530

    added user detail form

--- updateDetails.php
+++ updateDetails.php
@@ -6,8 +6,15 @@
     <div class="container borderBottom">
         <h2>EMAIL</h2>
         <input type="text" class="email" name="email" placeholder="Type your new email address here..." value="<?php echo $userLoggedIn->getEmail();?>">
+        <span class="message"></span>
+        <button class="button" onclick="">UPDATE EMAIL</button>
     </div>
     <div class="container">
         <h2>PASSWORD</h2>
+        <input type="password" class="oldPassword" name="oldPassword" placeholder="Current Password">
+        <input type="password" class="newPassword1" name="newPassword1" placeholder="New Password">
+        <input type="password" class="newPassword2" name="newPassword2" placeholder="Confirm Password">
+        <span class="message"></span>
+        <button class="button" onclick="">UPDATE EMAIL</button>
     </div>
 </div>
